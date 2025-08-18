96060bc3c3e80ce41c19dbd5edf993fe6cbf077a
ankamayi <drupalinside@gmail.com>
2025-08-18 18:43:21 +0900

    時間表示コンポーネントの追加

--- src/components/TimerApp.tsx
+++ src/components/TimerApp.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import TimerDisplay from './TimerDisplay';
 
 export default function TimerApp() {
     return (
@@ -10,7 +11,9 @@ export default function TimerApp() {
                 <CardHeader>
                     <CardTitle className='text-2xl font-bold text-center'>作業時間</CardTitle>
                 </CardHeader>
-                <CardContent>25:00</CardContent>
+                <CardContent className='flex justify-center'>
+                    <TimerDisplay minutes={25} seconds={0}/>
+                </CardContent>
             </Card>
         </div>
     )
